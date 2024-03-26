d41057a21c05015d9c6d5378678dd9976930dae0
Sumit Panchal <112932064+Sumitpanchal1993@users.noreply.github.com>
2024-03-26 23:38:00 +0530

    Correction in fonts

--- src/Assembly/Footer.jsx
+++ src/Assembly/Footer.jsx
@@ -2,7 +2,16 @@ import React from "react";
 // import './Footer.css'
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="footerBase">
+        <div>Quick Links</div>
+        <div>FOllow Us On</div>
+        <div>Manufaceturing Location</div>
+        <div>Download Browshur</div>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
